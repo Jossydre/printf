25b617fc79eb53af14db9547b724828952a20239
Jossydre <makinwadarejossy@gmail.com>
2023-09-18 01:47:47 -0700

    success

--- printf.c
+++ printf.c
@@ -8,6 +8,8 @@
 int _printf(const char *format, ...)
 {
 	int num = 0;
+	int str_len = 0;
+
 	va_list args;
 
 	if (*format == NULL)
@@ -17,13 +19,44 @@ int _printf(const char *format, ...)
 
 	while (*format)
 	{
-		if (*format != '\0')
+		if (*format != '%')
 		{
 			write(1, format, 1);
+			num ++;
 		}
+		else
+		{
+			format++;
 
-		format++;
+			if (*format == '\0')
+				break;
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				num++;
+			}
+
+			else if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+				write(1, &c, 1);
+				num++
+			}
+
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char*);
+				for (int i = 0; str[i] != '\0'; i++)
+				{
+					str_len++;
+					write(1, str, str_len);
+					num += str_len;
+				}
+			}
+			format++;
+		}
 	}
-
+	va_end(args);
 	return (num);
 }
+
