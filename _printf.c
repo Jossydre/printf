be93df9991c6ca2896a0decb59e0d82f145a49b6
root <akosabenedict@gmail.com>
2023-09-18 23:15:05 -0700

    printf

---
+++ _printf.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _printf - produces output accordng to format
+ * @format: format according to the argument to be printed
+ * Return: Number of printed character
+ */
+int _printf(const char *format, ...)
+{
+	int num_count = 0;
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while(*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+		}
+		format++;
+	}
+	return (num_count);
+}
---
+++ main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+int main()
+{
+	printf("Output Standard form\n");
+	printf("Hello, Team");
+
+	printf("\n____________________\n");
+
+	printf("Output locally\n");
+	_printf("Hello, Team\n");
+
+	return (0);
+}
